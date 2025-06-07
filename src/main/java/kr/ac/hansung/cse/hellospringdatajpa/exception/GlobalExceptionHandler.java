e910d078d9af20f1977efccf6f2bdde35e32779c
seojunyoung <aa2030822@naver.com>
2025-06-07 19:48:31 +0900

    hw2

--- src/main/java/kr/ac/hansung/cse/hellospringdatajpa/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package kr.ac.hansung.cse.hellospringdatajpa.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -8,10 +10,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @ExceptionHandler(UserAlreadyExistException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public String userAlreadyExistException(UserAlreadyExistException e) {
-        return e.getMessage();
+    public void userAlreadyExistException(UserAlreadyExistException e) {
+        logger.error(e.getMessage());
     }
 
+    @ExceptionHandler(NoRoleException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void noRoleException(NoRoleException e) {
+        logger.error(e.getMessage());
+    }
+
+    @ExceptionHandler(UserNameNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void userNameNotFoundException(UserNameNotFoundException e) {
+        logger.error(e.getMessage());
+    }
 }
